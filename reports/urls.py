7f7e2f438776fab91e7e8750f29e0ff9d8d7b172
LFJotaium <fluttercoding03@gmail.com>
2025-04-20 17:28:48 +0300

    BSPM25T29-30

--- reports/urls.py
+++ reports/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
       path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register_view, name='register'),
+          path('map/', views.map_view, name='map_view'),
+
 ]
