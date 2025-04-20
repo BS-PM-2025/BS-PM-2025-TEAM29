694ed292b6c8450374863f72152ab6c5dc39a061
1abrahem7 <162452286+1abrahem7@users.noreply.github.com>
2025-04-20 21:42:34 +0300

    BSPM25T29-3

--- reports/urls.py
+++ reports/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.report_list, name='report_list'),# Root URL for reports
     path('add/', views.add_report, name='add_report'),
+    path('confirmation/<str:report_id>/', views.report_confirmation, name='report_confirmation'), #Ibrahim BSPM25T29-3
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register_view, name='register'),
