a72bf56660620afd53883ca769643c3013202d75
LFJotaium <fluttercoding03@gmail.com>
2025-04-20 17:31:17 +0300

    BSPM25T29-30/33

--- reports/urls.py
+++ reports/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('login/', views.user_login, name='login'),
+    path('', views.report_list, name='report_list'),  # Root URL for reports
+    path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register_view, name='register'),
-          path('map/', views.map_view, name='map_view'),
+    path('map/', views.map_view, name='map_view'),
 
 ]
