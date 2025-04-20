6680c15425e80773b64ed144f4c49f3406419b4a
LFJotaium <fluttercoding03@gmail.com>
2025-04-20 17:07:16 +0300

    Remove
    START FROM SCRATCH

--- beer_sheva_backend/urls.py
+++ beer_sheva_backend/urls.py
@@ -9,10 +9,5 @@ initialize_firebase()
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.report_list, name='report_list'),  # List of reports
 
-    path('reports/', include('reports.urls')),  # Include the reports app's URLs
-
-    path('', lambda request: redirect('add_report')),  # Redirect root URL to 'add_report'
 ]
