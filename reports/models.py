25f3c3935c87d6b631d426cc09db8afb56871181
Firas <143230202+firasabs@users.noreply.github.com>
2025-04-20 19:33:02 +0300

    report model - under BSPM25T29-16

--- reports/models.py
+++ reports/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+class Report(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    place = models.CharField(max_length=255)  # 'place' for the location
+    latitude = models.FloatField()  # For latitude
+    longitude = models.FloatField()  # For longitude
+    report_time = models.DateTimeField(auto_now_add=True)  # Timestamp when the report is created
+    type = models.CharField(max_length=50 , default= 'לא ידוע')  # Simple string value for type
+    firebase_id = models.CharField(max_length=255, null=True, blank=True)  # Field for storing the Firebase ID
 
+    def __str__(self):
+        return self.title
 
 
