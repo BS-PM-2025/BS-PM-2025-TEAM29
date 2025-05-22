a69710a994118f2fea25f63abcfb340ffec4d90e
LFJotaium <fluttercoding03@gmail.com>
2025-05-22 14:33:31 +0300

    Update urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('add/', views.add_report, name='add_report'), #Firas BSPM25T29-16
     path('confirmation/<str:report_id>/', views.report_confirmation, name='report_confirmation'), #Ibrahim BSPM25T29-3
     path('map/', views.map_view, name='map_view'), #Malik
-    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'), #Abed BSPM25T29-17
     path('worker-dashboard/', views.worker_dashboard, name='worker_dashboard'), #Ibrahim BSPM25T29-151
     path("admin/dashboard/", admin_views.admin_dashboard, name="admin-firebase-reports")
 
