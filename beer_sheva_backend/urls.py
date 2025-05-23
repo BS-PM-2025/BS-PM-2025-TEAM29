6687fb23abb7c7b621bf45c198f93a3d44065870
firasabs <firasabosada@gmail.com>
2025-05-23 14:21:44 +0300

    design password_reset, password_reset_result

--- beer_sheva_backend/urls.py
+++ beer_sheva_backend/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', include('reports.urls')),
     path('user/', include('user.urls')),
     path('register/', user_views.register_view, name='register'),  # optional shortcut
-    path('login/', user_views.user_login, name='login'),            # optional shortcut
+    path('login/', user_views.user_login, name='login'),
+    path('reset_password/', user_views.reset_password_view, name='reset_password'),
 ]
