7c21ced7ee5f8325708e837f11479237e57cfb93
LFJotaium <fluttercoding03@gmail.com>
2025-06-11 23:51:38 +0300

    Update urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'), #Abed BSPM25T29-17
     path('worker-dashboard/', views.worker_dashboard, name='worker_dashboard'), #Ibrahim BSPM25T29-151
     path("admin/dashboard/", admin_views.admin_dashboard, name="admin-firebase-reports")
+    path('contact/', views.contact_us, name='contact_us'),
 
 ]
 
